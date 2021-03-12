1ab8860e1fd73bc7880bfedc9efb2da92af77bda
thesandx <kumarjha.sandeep@gmail.com>
2021-03-12 18:38:41 +0530

    minor changes

--- src/main/java/Project/Main.java
+++ src/main/java/Project/Main.java
@@ -21,18 +21,21 @@ public class Main {
                     double d = s.nextDouble();
                     System.out.println();
                     System.out.println("Answer is " + calculator.calculateRoot(d));
+                    System.out.println();
                     break;
                 case 2:
                     System.out.print("Enter value of x\n");
                     int d1 = s.nextInt();
                     System.out.println();
                     System.out.println("Answer is " + calculator.calculateFactorial(d1));
+                    System.out.println();
                     break;
                 case 3:
                     System.out.print("Enter value of x\n");
                     double d2 = s.nextDouble();
                     System.out.println();
                     System.out.println("Answer is " + calculator.calculateLog(d2));
+                    System.out.println();
                     break;
                 case 4:
                     System.out.print("Enter value of x\n");
@@ -42,6 +45,7 @@ public class Main {
                     double b = s.nextDouble();
                     System.out.println();
                     System.out.println("Answer is " + calculator.calculatePower(x,b));
+                    System.out.println();
                     break;
 
                 default:
