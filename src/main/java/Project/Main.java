84413fba0dfe411b33c0b53329ef3acae8f060ab
thesandx <kumarjha.sandeep@gmail.com>
2021-03-15 00:26:00 +0530

    Log file added

--- src/main/java/Project/Main.java
+++ src/main/java/Project/Main.java
@@ -1,9 +1,13 @@
 package Project;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(Main.class);
         while(true) {
             Menu menu = new Menu();
             menu.showMenu();
@@ -17,39 +21,56 @@ public class Main {
 
             switch (i) {
                 case 1:
+                    logger.info("Root calculation initiated"); //others are error/warn/fatal
                     System.out.print("Enter value of x\n");
                     double d = s.nextDouble();
+                    logger.info("value entered for root calculation is "+d);
                     System.out.println();
-                    System.out.println("Answer is " + calculator.calculateRoot(d));
+                    String ans  = calculator.calculateRoot(d);
+                    logger.info("answer is  "+ans);
+                    System.out.println("Answer is " + ans);
                     System.out.println();
                     break;
                 case 2:
+                    logger.info("factorial calculation initiated");
                     System.out.print("Enter value of x\n");
                     int d1 = s.nextInt();
+                    logger.info("value entered for factorial calculation is "+d1);
                     System.out.println();
-                    System.out.println("Answer is " + calculator.calculateFactorial(d1));
+                    String ans1  = calculator.calculateFactorial(d1);
+                    logger.info("answer is  "+ans1);
+                    System.out.println("Answer is " + ans1);
                     System.out.println();
                     break;
                 case 3:
+                    logger.info("Log calculation initiated");
                     System.out.print("Enter value of x\n");
                     double d2 = s.nextDouble();
+                    logger.info("value entered for log calculation is "+d2);
                     System.out.println();
-                    System.out.println("Answer is " + calculator.calculateLog(d2));
+                    String ans2 = calculator.calculateLog(d2);
+                    logger.info("answer is  "+ans2);
+                    System.out.println("Answer is " + ans2);
                     System.out.println();
                     break;
                 case 4:
+                    logger.info("Power calculation initiated");
                     System.out.print("Enter value of x\n");
                     double x = s.nextDouble();
                     System.out.println();
                     System.out.print("Enter value of b\n");
                     double b = s.nextDouble();
+                    logger.info("value entered for power calculation is "+x+" and "+b);
                     System.out.println();
+                    String ans3 = calculator.calculatePower(x,b);
+                    logger.info("answer is  "+ans3);
                     System.out.println("Answer is " + calculator.calculatePower(x,b));
                     System.out.println();
                     break;
 
                 default:
-                    System.out.print("kuch bhi\n");
+                    System.out.print("Invalid choice\n");
+                    logger.error("Invalid choice entered");
 
             }
         }
